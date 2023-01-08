f3819a60e8b89f832a2efcd94377b66f9ff5f939
Pranjal Chaplot <pranjalchaplot@gmail.com>
2023-01-08 13:20:44 +0530

    made option buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,39 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  void answerChosen() => print('answer chosen!');
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('good morning'),
+        ),
+        body: Column(
+          children: [
+            const Text("jai bajrang bali"),
+            ElevatedButton(
+              onPressed: answerChosen,
+              child: const Text('jai'),
+            ),
+            ElevatedButton(
+              onPressed: answerChosen,
+              child: const Text('shree'),
+            ),
+            ElevatedButton(
+              onPressed: answerChosen,
+              child: const Text('ram'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
